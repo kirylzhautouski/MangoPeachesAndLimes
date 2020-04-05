3658048c70e6484a2ea2ab61145318bd914e3a7b
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-05 14:19:08 +0300

    Create models draft

--- coctails/coctailsapi/models.py
+++ coctails/coctailsapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Drink(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # think about null blank unique here
+    is_alcoholic = models.BooleanField(default=False)
+    image_url = models.URLField()
+
+    # instruction string
+    # list of measures
+
+
+class Ingredient(models.Model):
+    pass
+
+
+class Measure(models.Model):
+    # contains ingredient and a measure of it
+    pass
