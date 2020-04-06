c1513b03a698dbc9e7be5f53bce8ec5e6cc8b670
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-06 14:37:01 +0300

    Create basic models

--- coctails/coctailsapi/models.py
+++ coctails/coctailsapi/models.py
@@ -2,18 +2,20 @@ from django.db import models
 
 
 class Drink(models.Model):
-    name = models.CharField(max_length=100, unique=True)  # think about null blank unique here
+    name = models.CharField(max_length=100, unique=True)
     is_alcoholic = models.BooleanField(default=False)
-    image_url = models.URLField()
-
-    # instruction string
-    # list of measures
+    image_url = models.URLField(blank=True)
+    instructions = models.TextField(blank=True)
 
 
 class Ingredient(models.Model):
-    pass
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    is_alcoholic = models.BooleanField(default=False)
 
 
 class Measure(models.Model):
-    # contains ingredient and a measure of it
-    pass
+    drink = models.ForeignKey(Drink, related_name='measures', on_delete=models.CASCADE)
+
+    ingredient = models.ForeignKey(Ingredient, related_name='measures', on_delete=models.CASCADE)
+    measure = models.CharField(max_length=50, blank=True)
