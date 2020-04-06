8295e89f34269ef83e8d7bf55ae002601d1f32f5
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-06 21:14:13 +0300

    Add measures of drinks

--- coctails/coctailsapi/serializers.py
+++ coctails/coctailsapi/serializers.py
@@ -1,17 +1,31 @@
 from rest_framework import serializers
 
-from coctailsapi.models import Drink, Ingredient
+from coctailsapi.models import Drink, Ingredient, Measure
+
+
+class MeasureSerializer(serializers.ModelSerializer):
+
+    ingredient = serializers.HyperlinkedRelatedField(view_name='ingredient-detail',
+                                                     queryset=Ingredient.objects.all())
+    ingredient_name = serializers.ReadOnlyField(source='ingredient.name')
+
+    class Meta:
+        model = Measure
+        fields = ['id', 'ingredient', 'ingredient_name', 'measure']
 
 
 class DrinkSerializer(serializers.HyperlinkedModelSerializer):
-    # TODO: add measures
+    measures = MeasureSerializer(many=True)
 
     class Meta:
         model = Drink
-        fields = ['url', 'id', 'name', 'is_alcoholic', 'image_url', 'instructions']
+        fields = ['url', 'id', 'name', 'is_alcoholic', 'image_url', 'instructions', 'measures']
 
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(view_name='coctailsapi:ingredient-detail')
+
     class Meta:
         model = Ingredient
         fields = ['url', 'id', 'name', 'description', 'is_alcoholic']
