5372b5513eddc2a018b7018990061d3cc02b2814
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-04-10 10:51:25 +0300

    Load data from API every midnight using Celery task

--- coctails/coctailsapi/tasks.py
+++ coctails/coctailsapi/tasks.py
@@ -1,7 +1,15 @@
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
+from django.core.management import call_command
 
 
+# executes daily at midnight
+@periodic_task(run_every=(crontab(minute=0, hour=0)))
+def load_data_periodic():
+    call_command('loaddata')
+
+
+# executes every minute
 @periodic_task(run_every=(crontab()))
 def hello():
-    print("Hello from Celery!")
+    print('Hello from Celery')
